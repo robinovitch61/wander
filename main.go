82a90402cc604defb9696d24e1f9d6e85f3744b9
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-01 21:49:31 -0700

    List nomad job names

--- main.go
+++ main.go
@@ -1,122 +1,135 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
-	"strings"
-
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 var (
-	titleStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
-	}()
-
-	infoStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Left = "┤"
-		return titleStyle.Copy().BorderStyle(b)
-	}()
+	NomadTokenEnvVariable = "NOMAD_TOKEN"
+	NomadUrlEnvVariable   = "NOMAD_URL"
 )
 
 type model struct {
-	content  string
-	ready    bool
-	viewport viewport.Model
+	nomadToken   string
+	nomadUrl     string
+	nomadJobList []string
+	err          error
+}
+
+// messages
+type nomadJobListMsg []string
+
+type errMsg struct{ err error }
+
+func (e errMsg) Error() string { return e.err.Error() }
+
+// commands
+func fetchJobNames(url, token string) tea.Cmd {
+	return func() tea.Msg {
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", fmt.Sprintf("%s/v1/jobs", url), nil)
+		if err != nil {
+			return nomadJobListMsg([]string{err.Error()})
+		}
+		req.Header.Set("X-Nomad-Token", token)
+		resp, err := client.Do(req)
+		if err != nil {
+			return nomadJobListMsg([]string{err.Error()})
+		}
+
+		type JobResponseEntry struct {
+			Name string
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nomadJobListMsg([]string{err.Error()})
+		}
+		var jobResponse []JobResponseEntry
+		if err := json.Unmarshal(body, &jobResponse); err != nil {
+			fmt.Println("Failed to decode response")
+		}
+
+		var jobs []string
+		for _, entry := range jobResponse {
+			jobs = append(jobs, entry.Name)
+		}
+		return nomadJobListMsg(jobs)
+	}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return fetchJobNames(m.nomadUrl, m.nomadToken)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmd  tea.Cmd
-		cmds []tea.Cmd
-	)
-
 	switch msg := msg.(type) {
+
+	case nomadJobListMsg:
+		m.nomadJobList = msg
+		return m, nil
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
 	case tea.KeyMsg:
-		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+		if msg.Type == tea.KeyCtrlC || msg.Type == tea.KeyEsc {
 			return m, tea.Quit
-		}
-
-	case tea.WindowSizeMsg:
-		headerHeight := lipgloss.Height(m.headerView())
-		footerHeight := lipgloss.Height(m.footerView())
-		verticalMarginHeight := headerHeight + footerHeight
-
-		if !m.ready {
-			// Since this program is using the full size of the viewport we
-			// need to wait until we've received the window dimensions before
-			// we can initialize the viewport. The initial dimensions come in
-			// quickly, though asynchronously, which is why we wait for them
-			// here.
-			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.viewport.YPosition = headerHeight
-			m.viewport.SetContent(m.content)
-			m.ready = true
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMarginHeight
 		}
 	}
 
-	// Handle keyboard and mouse events in the viewport
-	m.viewport, cmd = m.viewport.Update(msg)
-	cmds = append(cmds, cmd)
-
-	return m, tea.Batch(cmds...)
+	return m, nil
 }
 
 func (m model) View() string {
-	if !m.ready {
-		return "\n  Initializing..."
+	// If there's an error, print it out and don't do anything else.
+	if m.err != nil {
+		return fmt.Sprintf("\nError: %v\n\n", m.err)
 	}
-	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
-}
 
-func (m model) headerView() string {
-	title := titleStyle.Render("Mr. Pager")
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
-}
+	// Tell the user we're doing something.
+	s := "Loading..."
 
-func (m model) footerView() string {
-	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
+	// When the server responds with a status, add it to the current line.
+	if len(m.nomadJobList) > 0 {
+		s = ""
+		for _, job := range m.nomadJobList {
+			s += fmt.Sprintf("\n%s", job)
+		}
 	}
-	return b
+
+	// Send off whatever we came up with above for rendering.
+	return "\n" + s + "\n\n"
+}
+
+func initialModel() model {
+	nomadToken := os.Getenv(NomadTokenEnvVariable)
+	if nomadToken == "" {
+		fmt.Printf("Set environment variable %s", NomadTokenEnvVariable)
+		os.Exit(1)
+	}
+
+	nomadUrl := os.Getenv(NomadUrlEnvVariable)
+	if nomadUrl == "" {
+		fmt.Printf("Set environment variable %s", NomadUrlEnvVariable)
+		os.Exit(1)
+	}
+	return model{
+		nomadToken:   nomadToken,
+		nomadUrl:     nomadUrl,
+		nomadJobList: nil,
+		err:          nil,
+	}
 }
 
 func main() {
-	// Load some text for our viewport
-	//content, err := ioutil.ReadFile("artichoke.md")
-	content := ""
-	//if err != nil {
-	//	fmt.Println("could not load file:", err)
-	//	os.Exit(1)
-	//}
-
-	p := tea.NewProgram(
-		model{content: string(content)},
-		tea.WithAltScreen(),       // use the full size of the terminal in its "alternate screen buffer"
-		tea.WithMouseCellMotion(), // turn on mouse support so we can track the mouse wheel
-	)
-
-	if err := p.Start(); err != nil {
-		fmt.Println("could not run program:", err)
+	if err := tea.NewProgram(initialModel()).Start(); err != nil {
+		fmt.Printf("Error on wander startup: %v\n", err)
 		os.Exit(1)
 	}
 }
