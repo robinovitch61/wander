f6929fc6f9f8950969857dff5338e1bcdb51072c
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-14 12:03:44 -0700

    Start refactor

--- main.go
+++ main.go
@@ -10,12 +10,10 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"os"
-	"strings"
 	"wander/command"
 	"wander/components/header"
-	"wander/components/viewport"
+	"wander/components/jobs"
 	"wander/dev"
-	"wander/formatter"
 	"wander/message"
 	"wander/nomad"
 	"wander/page"
@@ -25,10 +23,6 @@ var (
 	NomadTokenEnvVariable = "NOMAD_TOKEN"
 	NomadUrlEnvVariable   = "NOMAD_ADDR"
 )
-
-type nomadJobData struct {
-	allData, filteredData []nomad.JobResponseEntry
-}
 
 type nomadAllocationData struct {
 	allData, filteredData []nomad.AllocationRowEntry
@@ -46,15 +40,13 @@ type model struct {
 	nomadToken          string
 	nomadUrl            string
 	keyMap              mainKeyMap
-	page                page.Page
+	currentPage         page.Page
+	jobsPage            jobs.Model
 	header              header.Model
-	viewport            viewport.Model
 	width, height       int
 	initialized         bool
-	nomadJobData        nomadJobData
 	nomadAllocationData nomadAllocationData
 	nomadLogData        nomadLogData
-	selectedJobId       string
 	selectedAlloc       selectedAlloc
 	logType             nomad.LogType
 	loading             bool
@@ -66,13 +58,13 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) fetchPageDataCmd() tea.Cmd {
-	switch m.page {
+	switch m.currentPage {
 
 	case page.Jobs:
 		return command.FetchJobs(m.nomadUrl, m.nomadToken)
 
 	case page.Allocation:
-		return command.FetchAllocations(m.nomadUrl, m.nomadToken, m.selectedJobId)
+		return command.FetchAllocations(m.nomadUrl, m.nomadToken, m.jobsPage.SelectedJobId)
 
 	case page.Logs:
 		return command.FetchLogs(m.nomadUrl, m.nomadToken, m.selectedAlloc.ID, m.selectedAlloc.taskName, m.logType)
@@ -81,7 +73,7 @@ func (m model) fetchPageDataCmd() tea.Cmd {
 }
 
 func (m *model) setHeaderKeyHelp() {
-	m.header.KeyHelp = KeyMapView(m.page, m.header.EditingFilter, m.header.HasFilter())
+	m.header.KeyHelp = KeyMapView(m.currentPage, m.header.EditingFilter, m.header.HasFilter())
 }
 
 func (m *model) setFiltering(isEditingFilter, clearFilter bool) {
@@ -93,81 +85,59 @@ func (m *model) setFiltering(isEditingFilter, clearFilter bool) {
 }
 
 func (m *model) setPage(p page.Page) {
-	m.page = p
+	m.currentPage = p
 	m.setHeaderKeyHelp()
 }
 
 func (m *model) setFilter(s string) {
 	m.header.Filter = s
-	m.viewport.Highlight = s
+	m.jobsPage.SetHighlightText(s)
 
-	switch m.page {
-	case page.Jobs:
-		m.updateJobViewport()
-	case page.Allocation:
-		m.updateAllocationViewport()
-	case page.Logs:
-		m.updateLogViewport()
-	}
+	//switch m.currentPage {
+	//case page.Jobs:
+	//	m.updateJobViewport()
+	//case page.Allocation:
+	//	m.updateAllocationViewport()
+	//case page.Logs:
+	//	m.updateLogViewport()
+	//}
 }
 
-func (m *model) updateViewport(table formatter.Table, cursorRow int) {
-	m.viewport.SetHeader(strings.Join(table.HeaderRows, "\n"))
-	m.viewport.SetContent(strings.Join(table.ContentRows, "\n"))
-	m.viewport.SetCursorRow(cursorRow)
-}
-
-func (m *model) updateFilteredJobData() {
-	var filteredJobData []nomad.JobResponseEntry
-	for _, entry := range m.nomadJobData.allData {
-		if entry.MatchesFilter(m.header.Filter) {
-			filteredJobData = append(filteredJobData, entry)
-		}
-	}
-	m.nomadJobData.filteredData = filteredJobData
-}
-
-func (m *model) updateJobViewport() {
-	m.updateFilteredJobData()
-	table := formatter.JobResponsesAsTable(m.nomadJobData.filteredData)
-	m.updateViewport(table, 0)
-}
-
-func (m *model) updateFilteredAllocationData() {
-	var filteredAllocationData []nomad.AllocationRowEntry
-	for _, entry := range m.nomadAllocationData.allData {
-		if entry.MatchesFilter(m.header.Filter) {
-			filteredAllocationData = append(filteredAllocationData, entry)
-		}
-	}
-	m.nomadAllocationData.filteredData = filteredAllocationData
-}
-
-func (m *model) updateAllocationViewport() {
-	m.updateFilteredAllocationData()
-	table := formatter.AllocationsAsTable(m.nomadAllocationData.filteredData)
-	m.updateViewport(table, 0)
-}
-
-func (m *model) updateFilteredLogData() {
-	var filteredLogData []nomad.LogRow
-	for _, log := range m.nomadLogData.allData {
-		if log.MatchesFilter(m.header.Filter) {
-			filteredLogData = append(filteredLogData, log)
-		}
-	}
-	m.nomadLogData.filteredData = filteredLogData
-}
-
-func (m *model) updateLogViewport() {
-	m.updateFilteredLogData()
-	table := formatter.LogsAsTable(m.nomadLogData.filteredData, m.logType)
-	m.updateViewport(table, len(table.ContentRows)-1)
-}
-
+//func (m *model) updateFilteredAllocationData() {
+//	var filteredAllocationData []nomad.AllocationRowEntry
+//	for _, entry := range m.nomadAllocationData.allData {
+//		if entry.MatchesFilter(m.header.Filter) {
+//			filteredAllocationData = append(filteredAllocationData, entry)
+//		}
+//	}
+//	m.nomadAllocationData.filteredData = filteredAllocationData
+//}
+//
+//func (m *model) updateAllocationViewport() {
+//	m.updateFilteredAllocationData()
+//	table := formatter.AllocationsAsTable(m.nomadAllocationData.filteredData)
+//	m.updateViewport(table, 0)
+//}
+//
+//func (m *model) updateFilteredLogData() {
+//	var filteredLogData []nomad.LogRow
+//	for _, log := range m.nomadLogData.allData {
+//		if log.MatchesFilter(m.header.Filter) {
+//			filteredLogData = append(filteredLogData, log)
+//		}
+//	}
+//	m.nomadLogData.filteredData = filteredLogData
+//}
+//
+//func (m *model) updateLogViewport() {
+//	m.updateFilteredLogData()
+//	table := formatter.LogsAsTable(m.nomadLogData.filteredData, m.logType)
+//	m.updateViewport(table, len(table.ContentRows)-1)
+//}
+//
 func (m *model) setLoading(loadingString string) {
 	m.loading = true
-	m.viewport.SetLoading(loadingString)
+	//m.viewport.SetLoading(loadingString)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -180,25 +150,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case message.NomadJobsMsg:
 		dev.Debug("nomadJobsMsg")
-		m.nomadJobData.allData = msg
-		m.updateJobViewport()
+		m.jobsPage, cmd = m.jobsPage.Update(msg)
+		cmds = append(cmds, cmd)
 		m.loading = false
-		return m, nil
 
-	case message.NomadAllocationMsg:
-		dev.Debug("NomadAllocationMsg")
-		m.nomadAllocationData.allData = msg
-		m.updateAllocationViewport()
-		m.loading = false
-		return m, nil
-
-	case message.NomadLogsMsg:
-		dev.Debug("NomadLogsMsg")
-		m.nomadLogData.allData = msg.Data
-		m.logType = msg.LogType
-		m.updateLogViewport()
-		m.loading = false
-		return m, nil
+	//case message.NomadAllocationMsg:
+	//	dev.Debug("NomadAllocationMsg")
+	//	m.nomadAllocationData.allData = msg
+	//	m.updateAllocationViewport()
+	//	m.loading = false
+	//	return m, nil
+	//
+	//case message.NomadLogsMsg:
+	//	dev.Debug("NomadLogsMsg")
+	//	m.nomadLogData.allData = msg.Data
+	//	m.logType = msg.LogType
+	//	m.updateLogViewport()
+	//	m.loading = false
+	//	return m, nil
 
 	case message.ErrMsg:
 		dev.Debug("errMsg")
@@ -239,15 +208,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 
 		case key.Matches(msg, m.keyMap.Forward):
-			switch m.page {
-			case page.Jobs:
-				m.selectedJobId = m.nomadJobData.filteredData[m.viewport.CursorRow].ID
-			case page.Allocation:
-				alloc := m.nomadAllocationData.filteredData[m.viewport.CursorRow]
-				m.selectedAlloc = selectedAlloc{alloc.ID, alloc.TaskName}
-			}
+			//switch m.currentPage {
+			//case page.Jobs:
+			//	m.selectedJobId = m.nomadJobData.filteredData[m.viewport.CursorRow].ID
+			//case page.Allocation:
+			//	alloc := m.nomadAllocationData.filteredData[m.viewport.CursorRow]
+			//	m.selectedAlloc = selectedAlloc{alloc.ID, alloc.TaskName}
+			//}
 
-			if newPage := m.page.Forward(); newPage != m.page {
+			if newPage := m.currentPage.Forward(); newPage != m.currentPage {
 				m.setFilter("")
 				m.setPage(newPage)
 				cmd := m.fetchPageDataCmd()
@@ -261,7 +230,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, nil
 			}
 
-			if newPage := m.page.Backward(); newPage != m.page {
+			if newPage := m.currentPage.Backward(); newPage != m.currentPage {
 				m.setPage(newPage)
 				cmd := m.fetchPageDataCmd()
 				m.setLoading(newPage.LoadingString())
@@ -270,45 +239,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		case key.Matches(msg, m.keyMap.Reload):
 			cmd := m.fetchPageDataCmd()
-			m.setLoading(m.page.ReloadingString())
+			m.setLoading(m.currentPage.ReloadingString())
 			return m, cmd
 
 		case key.Matches(msg, m.keyMap.Filter):
 			m.setFiltering(true, false)
 			return m, nil
 
-		case key.Matches(msg, m.keyMap.StdOut) && m.page == page.Logs:
+		case key.Matches(msg, m.keyMap.StdOut) && m.currentPage == page.Logs:
 			m.logType = nomad.StdOut
-			m.setLoading(m.page.LoadingString())
+			m.setLoading(m.currentPage.LoadingString())
 			return m, m.fetchPageDataCmd()
 
-		case key.Matches(msg, m.keyMap.StdErr) && m.page == page.Logs:
+		case key.Matches(msg, m.keyMap.StdErr) && m.currentPage == page.Logs:
 			m.logType = nomad.StdErr
-			m.setLoading(m.page.LoadingString())
+			m.setLoading(m.currentPage.LoadingString())
 			return m, m.fetchPageDataCmd()
-		}
-
-	case tea.WindowSizeMsg:
-		dev.Debug("WindowSizeMsg")
-		m.width, m.height = msg.Width, msg.Height
-
-		headerHeight := m.header.ViewHeight()
-		footerHeight := 0
-		viewportHeight := msg.Height - (headerHeight + footerHeight)
-
-		if !m.initialized {
-			// this is the first message received and initializes the viewport size
-			m.viewport = viewport.New(msg.Width, viewportHeight)
-			m.viewport.SetLoading(m.page.LoadingString())
-			m.initialized = true
-			return m, m.fetchPageDataCmd()
-		} else {
-			m.viewport.SetWidth(msg.Width)
-			m.viewport.SetHeight(viewportHeight)
 		}
 	}
 
-	m.viewport, cmd = m.viewport.Update(msg)
 	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
@@ -319,7 +268,12 @@ func (m model) View() string {
 		return fmt.Sprintf("\nError: %v\n\n", m.err)
 	}
 
-	finalView := m.header.View() + "\n" + m.viewport.View()
+	pageView := ""
+	switch m.currentPage {
+	case page.Jobs:
+		pageView = m.jobsPage.View()
+	}
+	finalView := m.header.View() + "\n" + pageView
 	return finalView
 }
 
@@ -343,13 +297,13 @@ func initialModel() model {
 		"▀▄▀▄▀ █▀█ █ ▀█ █▄▀ ██▄ █▀▄",
 	}
 	return model{
-		nomadToken: nomadToken,
-		nomadUrl:   nomadUrl,
-		keyMap:     keyMap,
-		logType:    nomad.StdOut,
-		page:       firstPage,
-		header:     header.New(logo, nomadUrl, KeyMapView(firstPage, false, false)),
-		loading:    true,
+		nomadToken:  nomadToken,
+		nomadUrl:    nomadUrl,
+		keyMap:      keyMap,
+		logType:     nomad.StdOut,
+		currentPage: firstPage,
+		header:      header.New(logo, nomadUrl, KeyMapView(firstPage, false, false)),
+		loading:     true,
 	}
 }
 
