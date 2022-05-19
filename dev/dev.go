0a0fd0de673822c67bf99ddaa8d48e4119bcaef3
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-18 18:45:55 -0700

    Turn off debug file creation by default

--- dev/dev.go
+++ dev/dev.go
@@ -7,13 +7,17 @@ import (
 	"os"
 )
 
+var debugSet = os.Getenv("WANDER_DEBUG")
+
 // dev
 func Debug(msg string) {
-	f, err := tea.LogToFile("debug.log", "")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
+	if debugSet != "" {
+		f, err := tea.LogToFile("wander.log", "")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		log.Printf("%q", msg)
+		defer f.Close()
 	}
-	log.Printf("%q", msg)
-	defer f.Close()
 }
