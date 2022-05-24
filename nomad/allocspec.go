7fa2d49eedf61343db74c4bb92bf6481de3b762c
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-23 17:44:16 -0700

    Add job and allocation specs (#16)
    * Disable cursor in logline page
    
    * Add jobspec, fix bugs
    
    * Add allocation spec

---
+++ nomad/allocspec.go
@@ -0,0 +1,32 @@
+package nomad
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"wander/components/page"
+	"wander/formatter"
+	"wander/message"
+)
+
+func FetchAllocSpec(url, token, allocID string) tea.Cmd {
+	return func() tea.Msg {
+		fullPath := fmt.Sprintf("%s%s%s", url, "/v1/allocation/", allocID)
+		body, err := get(fullPath, token, nil)
+		if err != nil {
+			return message.ErrMsg{Err: err}
+		}
+
+		pretty := formatter.PrettyJsonStringAsLines(string(body))
+
+		var allocSpecPageData []page.Row
+		for _, row := range pretty {
+			allocSpecPageData = append(allocSpecPageData, page.Row{Key: "", Row: row})
+		}
+
+		return PageLoadedMsg{
+			Page:        AllocSpecPage,
+			TableHeader: []string{},
+			AllPageData: allocSpecPageData,
+		}
+	}
+}
