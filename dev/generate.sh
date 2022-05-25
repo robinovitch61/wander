54cff1822656f11b16de465fda100400a1adb528
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-24 18:57:55 -0700

    Add image and gif link to readme (#17)
    * img
    
    * Update README.md

--- dev/generate.sh
+++ dev/generate.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-nomad job init
-sed -i "" -E "s/job \"example\"/job \"${1:-example}\"/" example.nomad
-nomad job run -detach example.nomad
-rm ./example.nomad
+run_job() {
+    nomad job init
+    sed -i "" -E "s/job \"example\"/job \"${1:-example}\"/" example.nomad
+    nomad job run -detach example.nomad
+    rm ./example.nomad
+}
+
+run_job alright_stop
+run_job collaborate_and_listen
+run_job ice_is_back
+run_job with_a_brand_new_invention
