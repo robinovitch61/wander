81dac80fb71404065125b28ec0a6536af6855645
Leo Robinovitch <leorobinovitch@gmail.com>
2022-05-26 09:57:01 -0700

    Consolidate formatting functions

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -1,9 +1,32 @@
 package formatter
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/olekukonko/tablewriter"
 	"strings"
+	"time"
 )
+
+func prettyPrint(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Indent(&out, b, "", "  ")
+	return out.Bytes(), err
+}
+
+func PrettyJsonStringAsLines(logline string) []string {
+	pretty, err := prettyPrint([]byte(logline))
+	if err != nil {
+		return []string{logline}
+	}
+
+	var splitLines []string
+	for _, row := range bytes.Split(pretty, []byte("\n")) {
+		splitLines = append(splitLines, string(row))
+	}
+
+	return splitLines
+}
 
 type Table struct {
 	HeaderRows, ContentRows []string
@@ -47,4 +70,29 @@ func GetRenderedTableAsString(columns []string, data [][]string) Table {
 	headerRows := []string{allRows[0]}
 	contentRows := allRows[1 : len(allRows)-1] // last row is \n
 	return Table{headerRows, contentRows}
+}
+
+func ShortAllocID(allocID string) string {
+	firstN := 8
+	if len(allocID) < firstN {
+		return ""
+	}
+	return allocID[:firstN]
+}
+
+func FormatTime(t time.Time) string {
+	if t.IsZero() {
+		return "-"
+	}
+	local, err := time.LoadLocation("Local")
+	if err != nil {
+		return "-"
+	}
+	tLocal := t.In(local)
+	return tLocal.Format("2006-01-02T15:04:05")
+}
+
+func FormatTimeNs(t int64) string {
+	tm := time.Unix(0, t).UTC()
+	return FormatTime(tm)
 }
