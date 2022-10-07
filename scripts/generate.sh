2f2331da309463e9778ecd5a1453ebff6fa3afe9
Leo Robinovitch <leorobinovitch@gmail.com>
2022-10-06 18:29:21 -0700

    Change cpu/memory for example jobs to avoid exceeding limits

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -9,6 +9,10 @@ sed -i "" -E '1a\
   namespace = "default"
 ' example.nomad
 
+# default cpu sometimes too high for local
+sed -i "" -E "s/cpu    = 500/cpu    = 50/g" example.nomad
+sed -i "" -E "s/memory = 256/memory = 32/g" example.nomad
+
 run_job() {
     sed -i "" -E "s/job \".*\"/job \"${1:-example}\"/" example.nomad
     sed -i "" -E "s/namespace = \".*\"/namespace = \"${2:-default}\"/" example.nomad
